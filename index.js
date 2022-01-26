60ce85c9dc44c4ffac7ac1396df2cb7399fbda33
kleitonalbuquerque <kleiton2102@gmail.com>
2022-01-26 10:05:18 -0300

    Relacionamento 1 para M

--- index.js
+++ index.js
@@ -161,17 +161,41 @@ console.log("*********************************************");
 
 // JOIN 1 p 1
 
+// database
+//   .select([
+//     "games.*",
+//     "estudios.id as estudio_id",
+//     "estudios.nome as estudio_nome",
+//   ])
+//   .table("games")
+//   .innerJoin("estudios", "estudios.game_id", "games.id")
+//   .where("games.id", 7)
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
 database
-  .select([
-    "games.*",
-    "estudios.id as estudio_id",
-    "estudios.nome as estudio_nome",
-  ])
+  .select(["games.*", "estudios.nome as estudio_nome"])
   .table("games")
   .innerJoin("estudios", "estudios.game_id", "games.id")
-  .where("games.id", 7)
+  .orderBy("estudios.game_id")
   .then((data) => {
-    console.log(data);
+    var game = {
+      id: 0,
+      nome: "",
+      estudios: [],
+    };
+
+    game.id = data[0].id;
+    game.nome = data[0].nome;
+
+    data.forEach((estudio) => {
+      game.estudios.push({ nome: estudio.estudio_nome });
+    });
+    console.log(game);
   })
   .catch((err) => {
     console.log(err);
